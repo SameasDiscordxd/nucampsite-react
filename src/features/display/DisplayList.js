0f125786b4c7d7e84a461daaa2fa78e455658b97
SameasDiscordxd <joshua.sartin.js@gmail.com>
2023-09-20 12:44:32 -0400

    "AnimatedDisplayCard".

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,8 +1,8 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 
 
 
@@ -14,9 +14,11 @@ const DisplayList = () => {
         <Row>
             {items.map((item, idx) => {
                 return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
+                    item && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={item} />
+                        </Col>
+                    )
                 );
             })}
         </Row>
