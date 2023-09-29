6e22846dc1aff44b2a98392619c3d47a604b574f
SameasDiscordxd <joshua.sartin.js@gmail.com>
2023-09-29 13:10:56 -0400

    "Add Middleware"

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import {campsitesReducer} from '../features/campsites/campsitesSlice';
+import logger from 'redux-logger';
+import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { commentsReducer } from '../features/comments/commentsSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
 
 export const store = configureStore({
-  reducer: {
-    campsites: campsitesReducer,
-  },
+    reducer: {
+        campsites: campsitesReducer,
+        comments: commentsReducer,
+        partners: partnersReducer,
+        promotions: promotionsReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
