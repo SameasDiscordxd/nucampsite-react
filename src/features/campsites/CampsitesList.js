2d2ce1719aeb830911c5bf4778b6846edb11edd3
SameasDiscordxd <joshua.sartin.js@gmail.com>
2023-10-06 06:41:36 -0400

    "Error and Loading"

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -3,28 +3,43 @@ import { Col, Row } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
 import { selectAllCampsites } from './campsitesSlice';
 import { useSelector } from 'react-redux';
+import Loading from '../../components/Loading';  // Imported Loading component
+import Error from '../../components/Error';      // Imported Error component
 
 const CampsitesList = () => {
-  const campsites = useSelector(selectAllCampsites);
-  console.log('campsites:', campsites);
+    const campsites = useSelector(selectAllCampsites);
 
+    // Add isLoading and errMsg using useSelector hook
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
 
-  return (
-    <Row className="ms-auto">
-      {campsites.map((campsite) => {
-        // Define a function to handle campsite clicks
+    // Check for loading state
+    if (isLoading) {
         return (
-          <Col
-            md="5"
-            className="m-4"
-            key={campsite.id}
-          >
-            <CampsiteCard campsite={campsite} />
-          </Col>
+            <Row>
+                <Loading />
+            </Row>
         );
-      })}
-    </Row>
-  );
+    }
+
+    // Check for error message
+    if (errMsg) {
+        return (
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        );
+    }
+
+    return (
+        <Row className="ms-auto">
+            {campsites.map((campsite) => (
+                <Col md="5" className="m-4" key={campsite.id}>
+                    <CampsiteCard campsite={campsite} />
+                </Col>
+            ))}
+        </Row>
+    );
 };
 
 export default CampsitesList;
--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Container, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
 import { selectCampsiteById } from "../features/campsites/campsitesSlice";
@@ -5,21 +6,37 @@ import CampsiteDetail from "../features/campsites/CampsiteDetail";
 import CommentsList from "../features/comments/CommentsList";
 import SubHeader from "../components/SubHeader";
 import { useSelector } from "react-redux";
+import Error from '../components/Error';
+import Loading from '../components/Loading';
 
 const CampsiteDetailPage = () => {
     const { campsiteId } = useParams();
     const campsite = useSelector(selectCampsiteById(campsiteId));
-    console.log("campsite:", campsite);
 
-  return (
-    <Container>
-      <Row>
-        <SubHeader current={campsite.name} detail={true} />
-        <CampsiteDetail campsite={campsite} />
-        <CommentsList campsiteId={campsiteId} />
-      </Row>
-    </Container>
-  );
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+
+    let content = null;
+
+    if (isLoading) {
+        content = <Loading />;
+    } else if (errMsg) {
+        content = <Error errMsg={errMsg} />;
+    } else {
+        content = (
+            <>
+                <SubHeader current={campsite.name} detail={true} />
+                <CampsiteDetail campsite={campsite} />
+                <CommentsList campsiteId={campsiteId} />
+            </>
+        );
+    }
+
+    return (
+        <Container>
+            <Row>{content}</Row>
+        </Container>
+    );
 };
 
 export default CampsiteDetailPage;
