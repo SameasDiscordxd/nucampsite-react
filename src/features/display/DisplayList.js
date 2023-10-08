d018814de1bdb833532e3f68711586f543c7231e
SameasDiscordxd <joshua.sartin.js@gmail.com>
2023-10-08 14:26:02 -0400

    "Async DisplayList"

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -4,7 +4,8 @@ import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
-
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 
 
@@ -18,16 +19,23 @@ const DisplayList = () => {
 
     return (
         <Row>
-            {items.map((item, idx) => {
-                return (
-                    item && (
-                        <Col md className='m-1' key={idx}>
-                            <AnimatedDisplayCard item={item} />
-                        </Col>
-                    )
-                );
-            })}
-        </Row>
+        {items.map((item, idx) => {
+            const { featuredItem, isLoading, errMsg } = item;
+            if (isLoading) {
+                return <Loading key={idx} />;
+            }
+            if (errMsg) {
+                return <Error errMsg={errMsg} key={idx} />;
+            }
+            return (
+                featuredItem && (
+                    <Col md className='m-1' key={idx}>
+                        <AnimatedDisplayCard item={featuredItem} />
+                    </Col>
+                )
+            );
+        })}
+    </Row>
     );
 };
 
--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -49,5 +49,11 @@ export const selectAllPartners = (state) => {
 };
 
 export const selectFeaturedPartner = (state) => {
-    return state.partners.partnersArray.find((partner) => partner.featured);
+    return {
+       featuredItem: state.partners.partnersArray.find(
+            (partner) => partner.featured
+        ),
+        isLoading: state.partners.isLoading,
+        errMsg: state.partners.errMsg
+    };
 };
